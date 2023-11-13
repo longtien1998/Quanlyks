4573be6c979a46a290dbfeee319cda6f6b998778
longtien1998 <125441317+longtien1998@users.noreply.github.com>
2023-11-13 22:00:32 +0700

    long tien

--- dao/room.php
+++ dao/room.php
@@ -58,3 +58,7 @@ function room_exist_LoaiPhong_update($maPhong, $LoaiPhong)
     $sql = "SELECT count(*) FROM room WHERE  maPhong!=? and LoaiPhong=?";
     return pdo_query_value($sql, $maPhong, $LoaiPhong) > 0;
 }
+{
+    $sql = "SELECT count(*) FROM room WHERE  maPhong!=? and LoaiPhong=?";
+    return pdo_query_value($sql, $maPhong, $LoaiPhong) > 0;
+}
